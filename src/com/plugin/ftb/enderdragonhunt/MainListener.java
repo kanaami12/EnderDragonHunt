cc0628198ae0772d27c8ad8572d41a7ff049e2d7
BuildTools <unconfigured@null.spigotmc.org>
2017-08-07 05:14:06 +0900

    ・ブレイズロッドを手に入れる ・エンダーパールを手に入れる ・エンダーアイを手に入れる ・エンダーアイを投げる ・エンドポータルを作る ・エンドクリスタルを壊す ・エンダードラゴンを倒す

--- src/com/plugin/ftb/enderdragonhunt/MainListener.java
+++ src/com/plugin/ftb/enderdragonhunt/MainListener.java
@@ -4,11 +4,27 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.EnderDragon;
+import org.bukkit.entity.EnderSignal;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockExplodeEvent;
 import org.bukkit.event.block.BlockMultiPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+
 public class MainListener implements Listener {
 
 	private String prefix = Main.prefix;
@@ -38,16 +54,31 @@ public class MainListener implements Listener {
 	}
 	
 	/*
-	 * エンダーポータルが開かれるとき、
+	 * アイテムを作ったとき、プレイヤーネームとアイテムを表示
+	 */
+	@EventHandler
+	public void onCreateItem(CraftItemEvent event) {
+		Player player = (Player) event.getWhoClicked();
+		Material material = event.getCurrentItem().getType();
+		String itemName = "";
+		
+		if(material.equals(Material.EYE_OF_ENDER)) {
+			itemName = ChatColor.GREEN + "エンダーアイ";
+			broadcast(prefix + " " + ChatColor.BOLD + player.getName() + ChatColor.RESET + "が"
+					+ itemName + ChatColor.RESET + "を作成した");
+		}
+	}
+	
+	/*
+	 * エンドポータルが開かれるとき、
 	 * プレイヤーネームと座標を表示
 	 */
 	@EventHandler
 	public void onPortal(BlockMultiPlaceEvent event) {
 		for(BlockState blockState : event.getReplacedBlockStates()) {
-			broadcast(blockState.getBlock().getType() + "");
 			if(blockState.getBlock().getType().equals(Material.ENDER_PORTAL)) {
 				broadcast(prefix + " " + ChatColor.BOLD + event.getPlayer().getName() + ChatColor.RESET + "が"
-						+ ChatColor.DARK_GREEN + "エンダーポータル" + ChatColor.RESET + "を開いた");
+						+ ChatColor.DARK_GREEN + "エンドポータル" + ChatColor.RESET + "を開いた");
 				broadcast(prefix + " 座標: " + event.getBlock().getLocation().getBlockX() + ", "
 						+ event.getBlock().getLocation().getBlockY() + ", "
 						+ event.getBlock().getLocation().getBlockZ() );
@@ -55,6 +86,68 @@ public class MainListener implements Listener {
 			}
 		}
 	}
+	
+	/*
+	 * エンダーパールが投げられたとき、通知する
+	 */
+	@EventHandler
+    public void onClick(PlayerInteractEvent event) {
+		if(event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+			ItemStack item = event.getItem();
+			if(item != null && item.getType().equals(Material.EYE_OF_ENDER)) {
+				Material material = event.getClickedBlock().getType();
+				if(material != null && material.equals(Material.ENDER_PORTAL_FRAME)) {
+					//エンダーポータルフレームに置いているのであれば無視
+					return;
+				}
+				broadcast(prefix + " " + ChatColor.BOLD + event.getPlayer().getName() + ChatColor.RESET + "が"
+						+ ChatColor.GREEN + "エンダーアイ" + ChatColor.RESET + "を投げた");
+			}
+		}
+	}
+	
+	/*
+	 * エンダークリスタルが爆発したとき通知する
+	 */
+	@EventHandler
+	public void onExplode(EntityDamageByEntityEvent event) {
+		Entity entity = event.getEntity();
+		if(entity.getType().equals(EntityType.ENDER_CRYSTAL)) {
+			Entity damager = event.getDamager();
+			if(damager.getType().equals(EntityType.ARROW)) {
+				Arrow arrow = (Arrow)damager;
+				if(arrow.getShooter() instanceof Player) {
+					//矢によって爆発した場合
+					Player player = (Player)arrow.getShooter();
+					broadcast(prefix + " " + ChatColor.BOLD + player.getName() + ChatColor.RESET + "が"
+							+ ChatColor.LIGHT_PURPLE + "エンダークリスタル" + ChatColor.RESET + "を射貫いて破壊した");
+					return;
+				}
+			}
+			if(damager instanceof Player) {
+				Player player = (Player)damager;
+				broadcast(prefix + " " + ChatColor.BOLD + player.getName() + ChatColor.RESET + "が"
+						+ ChatColor.LIGHT_PURPLE + "エンダークリスタル" + ChatColor.RESET + "を破壊した");
+				return;
+			}
+		}
+	}
+	
+	/*
+	 * エンダードラゴンが倒されたとき、通知する
+	 */
+	@EventHandler
+	public void onDeath(EntityDeathEvent event) {
+		String playerName = "";
+		if(event.getEntity().getKiller() != null) {
+			playerName =  ChatColor.BOLD + event.getEntity().getKiller().getName() + ChatColor.RESET + "が";
+		}
+		if(event.getEntity() instanceof EnderDragon) {
+			broadcast(prefix +  " " + playerName + ChatColor.DARK_PURPLE + "エンダードラゴン" + ChatColor.RESET + "を倒した");
+		}
+	}
+	
+	
 	/*
 	 * 拾ったアイテムを通知
 	 * @param itemName 拾ったアイテムの名前
