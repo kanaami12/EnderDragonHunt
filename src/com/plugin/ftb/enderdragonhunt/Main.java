2d9213f22265d66e5cc7e6c4827efaf24008bb1f
EruJapan <erunocyanu@gmail.com>
2017-08-07 14:15:58 +0900

    Update Main.java

--- src/com/plugin/ftb/enderdragonhunt/Main.java
+++ src/com/plugin/ftb/enderdragonhunt/Main.java
@@ -22,5 +22,70 @@ public class Main extends JavaPlugin{
 
 		//タブ補完登録
 		getCommand("enderdragon").setTabCompleter(new MainTabCompleter());
+		
+		//クラス作成がめんどくさかったので直接入れちゃいました
+		//もしクラス分けするときは消してください！by える
+		BukkitScheduler scheduler = Bukkit.getServer().getScheduler();
+		scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
+			@Override
+			public void run() {
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					Biome biome = p.getWorld().getBiome(p.getLocation().getBlockX(), p.getLocation().getBlockZ());
+					if (biome == Biome.HELL && MainListener.cnetheryousai == false) {
+						Location loc = p.getLocation();
+						for (int x = 0; x <= 6; x += 2) {
+							for (int y = 0; y <= 6; y += 2) {
+								for (int z = 0; z <= 6; z += 2) {
+									Location newloc = new Location(p.getWorld(), loc.getX() + (x - 3),
+											loc.getY() + (y - 3), loc.getZ() + (z - 3));
+									Block b = newloc.getBlock();
+									Material mat = b.getType();
+									if (mat == Material.NETHER_BRICK) {
+										if (!MainListener.cnetheryousai) {
+											MainListener.cnetheryousai = true;
+											p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
+											int x1 = p.getLocation().getBlockX();
+											int y1 = p.getLocation().getBlockY();
+											int z1 = p.getLocation().getBlockZ();
+											Bukkit.broadcastMessage("[" + ChatColor.DARK_RED + "ネザー" + ChatColor.RESET
+													+ "]" + " " + ChatColor.BOLD + p.getName() + ChatColor.RESET + "さんが"
+													+ ChatColor.DARK_RED + "ネザー要塞" + ChatColor.RESET + "を見つけた(座標: " + x1
+													+ ", " + y1 + ", " + z1 + ")");
+										}
+
+									}
+								}
+							}
+						}
+					} else if (biome != Biome.HELL && MainListener.cendpotal == false) {
+						Location loc = p.getLocation();
+						for (int x = 0; x <= 6; x += 2) {
+							for (int y = 0; y <= 6; y += 2) {
+								for (int z = 0; z <= 6; z += 2) {
+									Location newloc = new Location(p.getWorld(), loc.getX() + (x - 3),
+											loc.getY() + (y - 3), loc.getZ() + (z - 3));
+									Block b = newloc.getBlock();
+									Material mat = b.getType();
+									if (mat == Material.ENDER_PORTAL_FRAME) {
+										if (!MainListener.cendpotal) {
+											MainListener.cendpotal = true;
+											p.playSound(p.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 1.0f, 1.0f);
+											int x1 = p.getLocation().getBlockX();
+											int y1 = p.getLocation().getBlockY();
+											int z1 = p.getLocation().getBlockZ();
+											Bukkit.broadcastMessage(
+													"[" + ChatColor.DARK_GREEN + "ポータル" + ChatColor.RESET + "]" + " "
+															+ ChatColor.BOLD + p.getName() + ChatColor.RESET + "さんが"
+															+ ChatColor.DARK_GREEN + "エンドポータル" + ChatColor.RESET
+															+ "を見つけた(座標: " + x1 + ", " + y1 + ", " + z1 + ")");
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}, 0L, 60L);
 	}
 }
